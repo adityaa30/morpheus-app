d7d63941b91f750e15eefcc1c22e2287c412f83e
adityaa30 <k.aditya00@gmail.com>
2019-01-19 12:06:36 +0530

    Feat: Add Splash, Main activity.

--- app/src/main/java/com/morpheus/aditya/drive/HomeActivity.kt
+++ app/src/main/java/com/morpheus/aditya/drive/HomeActivity.kt
@@ -1,12 +1,21 @@
 package com.morpheus.aditya.drive
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.card.MaterialCardView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var mAlertMaterialCard: MaterialCardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        mAlertMaterialCard = findViewById(R.id.home_alert_materialCard)
+    }
+
+    private fun startAlertForegroundService() {
+        
     }
 }
